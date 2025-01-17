b8245e6955d69d9350992f27cc233311203136ba
Constanza Zúñiga <cons.zunigap@duocuc.cl>
2025-01-17 13:04:29 -0300

    Commit inicial

---
+++ botApp/urls.py
@@ -0,0 +1,49 @@
+from django.urls import path, include
+from . import views
+from .views import *
+
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register(r'Usuario', views.UsuarioViewSet)
+router.register(r'UsuarioRespuesta', views.UsuarioRespuestaViewSet)
+router.register(r'UsuarioTextoPregunta', views.UsuarioTextoPreguntaViewSet)
+router.register(r'MensajeContenido', views.MensajeContenidoViewSet)
+
+urlpatterns = [
+    path('', views.home),
+    path('home/', views.home, name='home'),
+    path('login/', views.login, name='login'),
+    path('reportes/', views.reportes, name='reportes'),
+    path('formulario/', views.formulario, name='formulario'),
+    
+    # Respuestas
+    path('respuestas/', views.respuestasHome, name='respuestasHome'),
+    path('datosPerfil/', views.datosPerfil, name='datosPerfil'),
+    path('datosPreguntas/', views.datosPreguntas, name='datosPreguntas'),
+    path('datosTextoPreguntas/', views.datosTextoPreguntas, name='datosTextoPreguntas'),
+    
+    # Preguntas
+    path('listarPreguntas/', views.listarPreguntas, name='listarPreguntas'),
+    path('modificarPregunta/<id>/', views.modificarPregunta, name='modificarPregunta'),
+    path('eliminarPregunta/<id>/', views.eliminarPregunta, name='eliminarPregunta'),
+    path('crearPregunta/', views.crearPregunta, name='crearPregunta'),
+
+    # Descargar Excel
+    path('descargar_excel/', views.descargar_excel, name='descargar_excel'),
+    
+    # Mensajes
+    path('home_mensajes/', views.homeMensajes, name='mensajesHome'),
+    path('crearMensaje/', views.crearMensaje, name='crearMensaje'),
+    path('modificarMensaje/<id>/', views.modificarMensaje, name='modificarMensaje'),
+    path('eliminarMensaje/<id>/', views.eliminarMensaje, name='eliminarMensaje'),
+
+    # API
+    path('api/', include(router.urls)),
+    path('apiHome/', apiHome, name='apiHome'),
+    path('obtener-id/', ObtenerID.as_view(), name='obtener_id'),    
+    path('api_usuario/', UsuarioAPIView.as_view(), name='api_usuario'),
+    path('api_pregunta/', UsuarioTextoPreguntaAPIView.as_view(), name='api_pregunta'),
+    path('api_respuesta/', UsuarioRespuestaAPIView.as_view(), name='api_respuesta'),
+    path('api_mensaje/', MensajeContenidoAPIView.as_view(), name='api_mensaje'),
+]
