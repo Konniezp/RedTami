9aa0fc581964db73cd394569c4a83aae210771db
Constanza Zúñiga <cons.zunigap@duocuc.cl>
2025-02-24 18:35:12 -0300

    jkasd

--- botApp/views.py
+++ botApp/views.py
@@ -2343,7 +2343,7 @@ def consultar_estado_pregunta(request, Rut, tipo_pregunta, nombre_pregunta):
             id_pregunta = pregunta_model.id
 
             opcion_respuesta_model = list(PreguntaOpcionRespuesta.objects.filter(id_pregunta=id_pregunta).values_list("id", flat=True))
-            return JsonResponse({"lista": opcion_respuesta_model})
+            #return JsonResponse({"lista": opcion_respuesta_model})
             respuesta = UsuarioRespuesta.objects.filter(Rut=data["Rut"], id_opc_respuesta__in=opcion_respuesta_model)
             return JsonResponse(respuesta)
         elif data["tipo_pregunta"] == "YYY":
